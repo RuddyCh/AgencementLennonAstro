780de30359b53e1bdf4cf7c9114b85d6b8d38a54
RuddyCh <ruddy-chabrat@laposte.net>
2026-04-26 15:12:39 +0200

    sitemap update correction

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '002fdcf56b6a127c86988d9614028e0b26e50750', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/RudBi/Downloads/agencement-lennon-astro/agencement-lennon/tina/__generated__/.cache/1777209113573', url: 'https://content.tinajs.io/2.2/content/8b10616a-6e55-4520-b1dc-7f8a98566ac5/github/main', token: '002fdcf56b6a127c86988d9614028e0b26e50750', queries,  });
 export default client;
   
